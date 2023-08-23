ba974b3835c6c4726cd7c0443425b8b1cd324b39
emattiza <emattiza@gmail.com>
2023-08-23 13:45:38 +0000

    add basic start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "shorte_rs=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    info!("hello, web server!");
 }
