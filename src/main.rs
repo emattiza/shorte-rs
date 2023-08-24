559a1bfc93fa9e75168e8edabb3b5bce767a32ea
emattiza <emattiza@gmail.com>
2023-08-24 00:35:14 +0000

    another step down

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use anyhow::Context;
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+};
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -10,5 +17,40 @@ async fn main() {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
-    info!("hello, web server!");
+    info!("initializing router");
+    let router = axum::Router::new().route("/", get(hello));
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 51420));
+    info!("server started on {}", &addr);
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .context("error while starting server")?;
+    Ok(())
+}
+
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> axum::response::Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
